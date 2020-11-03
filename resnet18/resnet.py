617fb53bbd6559db2f5ff58b15534450cdfdfb39
SmartPolarBear <clevercoolbear@outlook.com>
2020-11-03 18:38:10 +0800

    add trt_converter

--- resnet18/resnet.py
+++ resnet18/resnet.py
@@ -6,8 +6,17 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 import torchvision.transforms as transforms
 
+from os import listdir, getcwd
+from os.path import isfile, join, splitext
+
+if torch.cuda.is_available() == False:
+    print("No CUDA support.")
+    exit()
+
+dataset_path = join(getcwd(), "resnet18/dataset")
+
 dataset = datasets.ImageFolder(
-    'dataset',
+    dataset_path,
     transforms.Compose([
         transforms.ColorJitter(0.1, 0.1, 0.1, 0.1),
         transforms.Resize((224, 224)),
@@ -16,14 +25,17 @@ dataset = datasets.ImageFolder(
     ])
 )
 
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset) - 50, 50])
+train_dataset, test_dataset = torch.utils.data.random_split(
+    dataset, [len(dataset) - 50, 50])
+
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=8,
+    batch_size=16,
     shuffle=True,
     num_workers=0,
 )
+
 
 test_loader = torch.utils.data.DataLoader(
     test_dataset,
@@ -32,41 +44,40 @@ test_loader = torch.utils.data.DataLoader(
     num_workers=0,
 )
 
-model = models.resnet18(pretrained=True)
-
+model = models.resnet18(pretrained=True).cuda()
 model.fc = torch.nn.Linear(512, 4)
 
-device = torch.device('cuda')
+device = torch.device('cuda:0')
 model = model.to(device)
 
-NUM_EPOCHS = 30
+NUM_EPOCHS = 15
 BEST_MODEL_PATH = 'best_model_resnet18.pth'
 best_accuracy = 0.0
 
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 for epoch in range(NUM_EPOCHS):
-    
+
     for images, labels in iter(train_loader):
         images = images.to(device)
         labels = labels.to(device)
+
         optimizer.zero_grad()
         outputs = model(images)
         loss = F.cross_entropy(outputs, labels)
         loss.backward()
         optimizer.step()
-    
+
     test_error_count = 0.0
     for images, labels in iter(test_loader):
         images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
-        test_error_count += float(torch.sum(torch.abs(labels - outputs.argmax(1))))
-    
+        test_error_count += float(torch.sum(torch.abs(labels -
+                                                      outputs.argmax(1))))
+
     test_accuracy = 1.0 - float(test_error_count) / float(len(test_dataset))
     print('Epoch %d: accuracy %f' % (epoch, test_accuracy))
     if test_accuracy > best_accuracy:
         torch.save(model.state_dict(), BEST_MODEL_PATH)
         best_accuracy = test_accuracy
-
-
---
+++ trt/trt_convert.py
@@ -0,0 +1,17 @@
+import torch
+import torchvision
+from torch2trt import torch2trt
+
+model = torchvision.models.resnet18(pretrained=False)
+model.fc = torch.nn.Linear(512, 2)
+model = model.cuda().eval().half()
+
+model.load_state_dict(torch.load('best_model_resnet18.pth'))
+
+device = torch.device('cuda')
+
+data = torch.zeros((1, 3, 224, 224)).cuda().half()
+
+model_trt = torch2trt(model, [data], fp16_mode=True)
+
+torch.save(model_trt.state_dict(), 'best_model_trt.pth')
